6280c3f6e6587a0cb578ef8beaef687662ec227e
Arora <su238231b4e9@rpi-buster-armhf>
2023-06-18 19:36:37 +0000

    Create hello world program in ARM

---
+++ modules/module3/HelloWorld.s
@@ -0,0 +1,16 @@
+.text
+.global main
+main:
+    SUB sp, sp, #4
+    STR lr, [sp]
+    
+    LDR r0, =helloworld
+    BL printf     
+
+    LDR lr, [sp]
+    ADD sp, sp, #4
+    MOV pc, lr
+
+.data
+    helloworld: .asciz "Hello, World!\n"
+# End main
