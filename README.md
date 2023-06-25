36f902bc0d6f16db80415c02ec29ed8236ce9d34
karnika <arorakarnika@gmail.com>
2023-06-25 18:17:35 -0400

    module 4 assignment - age output

---
+++ README.md
@@ -0,0 +1,3 @@
+# Description
+
+A repository for assignments completed as part of JHU's Cmputer Organization class, Summer 2023.
---
+++ assignments/module4/printAgeAssignmentKA.s
@@ -0,0 +1,37 @@
+# Name: Karnika Arora
+# A program to ask a user for their age and output it
+
+.text
+.global main
+main:
+    # Push the stack
+    SUB sp, sp, #4
+    STR lr, [sp, #0]
+
+    # Print prompt asking for user's age
+    LDR r0, =agePrompt
+    BL printf
+
+    # Read in user inputted age
+    LDR r0, =formatAge
+    LDR r1, =age1
+    BL scanf   
+
+    # print output
+    LDR r0, =output
+    LDR r1, =age1
+    LDR r1, [r1, #0]
+    BL printf
+
+    # Pop the stack and return
+    LDR lr, [sp, #0]
+    ADD sp, sp. #4
+    MOV pc, lr
+
+.data
+    agePrompt: .asciz "Enter your age: \n"
+    formatAge: .asciz "%d"
+    age1: .word 0
+    output: .asciz "Your age is %d"
+
+
