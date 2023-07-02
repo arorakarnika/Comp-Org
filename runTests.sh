feebb5f85a685f7cfda211ea91d1da492856cb4e
karnika <arorakarnika@gmail.com>
2023-07-02 18:25:29 -0400

    create util bash scripts

---
+++ runTests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+while getopts n:t: flag
+do
+ case "${flag}" in
+        n) progName=${OPTARG};;
+        t) tests+=${OPTARG};;
+    esac
+done
+shift $((OPTIND -1))
+echo "Running $progName with test cases: $tests";
+
+for test in "${tests[@]}"; do
+    echo $test | ./progName
+done
