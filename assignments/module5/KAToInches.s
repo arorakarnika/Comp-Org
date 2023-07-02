49c3af2a02ea23a60ebf34e7f9ac08f03be0a88e
karnika <arorakarnika@gmail.com>
2023-07-02 17:19:40 -0400

    Create initial program

--- assignments/module5/KAToInches.s
+++ assignments/module5/KAToInches.s
@@ -1,3 +1,51 @@
-# 3 - Read two input numbers that represent feet and inches and convert this answer to be just inches.
+# Name: Karnika Arora
+# Module 5 Assignment
+# Date: 07/02/2023
+# 3a) - Read two input numbers that represent feet and inches and convert this answer to be just inches.
 # Then write a second program to do the reverse, taking one input in inches and convert it to feet and inches 
 # (this requires a remainder operation, that is not available on the Pi, so you have to write your own).
+
+.text
+.global main
+main:
+    # Push the stack
+    SUB sp, sp, #4 
+    STR lr, [sp, #0] 
+
+    # Print prompt for two input numbers
+    LDR r0, =prompt1
+    BL printf
+
+    # Read in the input values
+    LDR r0, =formatStr
+    LDR r1, =inFeet
+    LDR r2, =inInches
+    BL scanf
+
+    # Convert feet to inches
+    LDR r1, =inFeet
+    LDR r2, [r2]
+    MUL r0, r2, #12
+
+    # Add inches to the converted value to get final result
+    MOV r1, r0
+    LDR r2, =inInches
+    LDR r3, [r2]
+    ADD r0, r1, r3
+
+    # Print the final value
+    MOV r1, r0
+    LDR r0, =outStr
+    BL printf
+
+    # Pop the stack and return
+    LDR lr, [sp, #0] 
+    ADD sp, sp, #4 
+    MOV pc, lr 
+
+.data
+    prompt1: .asciz "Enter a number in feet followed by a number in inches, separated by a comma:\n"
+    formatStr: .asciz "%d, %d"
+    inFeet: .word 0
+    inInches: .word 0
+    outStr: .asciz "The value in inches is %d\n"
