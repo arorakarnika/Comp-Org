5e43c53cdaa39d2ba3c0d84ae27df12cbd66ba6d
arorakarnika <arorakarnika@gmail.com>
2023-07-02 23:03:05 +0000

    fix testing script by adding parentheses to record array input

--- runTests.sh
+++ runTests.sh
@@ -4,12 +4,12 @@ while getopts n:t: flag
 do
  case "${flag}" in
         n) progName=${OPTARG};;
-        t) tests+=${OPTARG};;
+	t) tests=(${OPTARG});;
     esac
 done
 shift $((OPTIND -1))
-echo "Running $progName with test cases: $tests";
+echo "Running $progName with test cases: ${tests[@]}";
 
 for test in "${tests[@]}"; do
-    echo $test | ./progName
+    echo $test | ./$progName
 done
