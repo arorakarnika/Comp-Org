2bd4aff8bf8fd1f54bf56453ba2f3a8a550218fc
arorakarnika <arorakarnika@gmail.com>
2023-07-03 02:52:51 +0000

    add space between args

--- assignments/module5/module5tests.sh
+++ assignments/module5/module5tests.sh
@@ -8,6 +8,6 @@ echo `make All`
 ../../runTests.sh -n KATempToCelsius -t "90, 95, 104"
 ../../runTests.sh -n KATempToF -t "32, 26, 18"
 ../../runTests.sh -n KAIntToNegative -t "5, 45, 10"
-../../runTests.sh -n KAToInches -t "6'2\",5'5\",4'11\""
+../../runTests.sh -n KAToInches -t "6'2\", 5'5\", 4'11\""
 ../../runTests.sh -n KAInchesToFeet -t "72, 65, 62"
 ../../runTests.sh -n KAMultiplyByTen -t "3, 4, 6"
