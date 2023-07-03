7e4571be6d88613137e6c3a86607bc60c7668174
arorakarnika <arorakarnika@gmail.com>
2023-07-03 03:14:41 +0000

    fix seg fault due to missing ldr instruction

--- assignments/module5/KASwap.s
+++ assignments/module5/KASwap.s
@@ -23,8 +23,12 @@ main:
     BL scanf
 
     # Swap the values
+    LDR r1, =input1
+    LDR r2, =input2
     LDR r1, [r1]
     LDR r2, [r2]
+    EOR r1, r1, r2
+    EOR r2, r2, r1
     EOR r1, r1, r2
     LDR r0, =outStr
     BL printf
@@ -38,5 +42,5 @@ main:
     prompt: .asciz "Enter two comma separated numbers to swap (e.g. 2,3): "
     formatIn: .asciz "%d,%d"
     input1: .word 0
-    input2 .word 0
-    outStr: .asciz "The swapped values are %d,%d"
+    input2: .word 0
+    outStr: .asciz "The swapped values are %d,%d\n"
