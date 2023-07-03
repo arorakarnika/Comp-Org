42f4f6248324e899c5c62fe0ffb9a014b1a1e837
karnika <arorakarnika@gmail.com>
2023-07-02 20:43:23 -0400

    Update comments

--- assignments/module5/module5tests.sh
+++ assignments/module5/module5tests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
-# Make all programs in dir
+# Author: Karnika Arora
+# Make all programs in the current directory
 
 echo `make All`
 
-# Run tests
+# Run tests, runTests.sh should be two directories up from the current directory
 ../../runTests.sh -n KATempToCelsius -t "90, 95, 104"
 ../../runTests.sh -n KATempToF -t "32, 26, 18"
 ../../runTests.sh -n KAIntToNegative -t "5, 45, 10"
--- runTests.sh
+++ runTests.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Author: Karnika Arora
+# This script should be called with the -n and -t flags e.g. ./runTests.sh -n <prog_name> -t <test_cases>
+# This script currently only works with single input test cases, and may be updated in the future to support multiple inputs
 
 while getopts n:t: flag
 do
