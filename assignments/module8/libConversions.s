9a8c4ca660a2b5b5fa106c0278a9a3346f3d7bd5
karnika <arorakarnika@gmail.com>
2023-07-23 18:33:05 -0400

    Create Module 8 Assignment Files

---
+++ assignments/module8/libConversions.s
@@ -0,0 +1,69 @@
+# Program Name: libConversions.s
+# Author: Karnika Arora
+# Date: 07/23/2023
+# Module 8 Assignment
+# Purpose: Create a library of functions for use by other programs
+# Functions: miles2kilometer(int miles), kph(int hours, int miles)
+# Inputs: (when applicable)
+# Outputs: (when applicable)
+
+.global miles2kilometer
+.global kph
+
+.text
+
+# START miles2kilometer
+
+# Create a function miles2kilometer that takes a number of miles and returns the number of kilometers
+# The function will multiply the number of miles by 161 and then divide by 100 to get the number of kilometers
+# This is done for simplicity and to avoid using floating point numbers, we would achieve greater precision by multiplying by 1.61
+
+miles2kilometer:
+
+    # Push the stack
+    SUB sp, sp, #4 
+    STR lr, [sp, #0] 
+
+    // Multiply miles by 161
+    MOV r1, #161
+    MUL r0, r0, r1
+
+    // Divide the result by 100
+    MOV r1, #100
+    BL __aeabi_idiv
+
+    # Pop the stack and return
+    LDR lr, [sp, #0] 
+    ADD sp, sp, #4 
+    MOV pc, lr 
+
+.data
+# END miles2kilometer
+
+# START kph
+
+# Create a function kph that takes a number of hours and a number of miles and returns the number of kilometers per hour
+# The function will call miles2kilometer to convert the number of miles to kilometers
+
+.text
+kph:
+
+    # Push the stack
+    SUB sp, sp, #4 
+    STR lr, [sp, #0] 
+
+    // Convert miles to kilometers using miles2kilometer function
+    // The input miles is in r0
+    BL miles2kilometer // result will be in r0
+
+    // Divide kilometers by hours
+    // hours is the input in r1
+    BL __aeabi_idiv
+
+    # Pop the stack and return
+    LDR lr, [sp, #0] 
+    ADD sp, sp, #4 
+    MOV pc, lr 
+
+.data
+# END kph
---
+++ assignments/module8/main1.s
@@ -0,0 +1,62 @@
+# Program Name: main1.s
+# Author: Karnika Arora
+# Date: 07/23/2023
+# Module 8 Assignment
+# Purpose: Use functions from libConversions.s to convert miles to kilometers, and miles and hours to kilometers per hour
+# Inputs: miles, hours
+# Outputs: miles, miles per hour
+
+.global main
+main:
+    // Push the stack
+    SUB sp, sp, #4 
+    STR lr, [sp, #0]
+
+    // Print prompt for two input numbers
+    LDR r0, =prompt1
+    BL printf
+
+    // Read in user input for miles and hours
+    LDR r0, =formatStr
+    LDR r1, =inMiles
+    LDR r2, =inHours
+    BL scanf
+
+    // Convert miles to kilometers
+    LDR r0, =inMiles
+    LDR r0, [r0]
+    BL miles2kilometer // now r0 has the kilometer value
+
+    // Store the kilometer value in outKilometers
+    // It will be printed at the end with the kilometers per hour value
+    LDR r1, =outKilometers
+    STR r1, [r0]
+
+    // Convert miles and hours to kilometers per hour
+    LDR r0, =inMiles
+    LDR r1, =inHours
+    BL kph
+
+
+    // Print result
+    MOV r4, r0 // move the kph value into r4 for the output string format
+    LDR r0, =outStr
+    LDR r1, =inMiles
+    LDR r2, =outKilometers
+    LDR r3, =inHours
+    BL printf
+
+
+    # Pop the stack and return
+    LDR lr, [sp, #0] 
+    ADD sp, sp, #4 
+    MOV pc, lr 
+
+.data
+    prompt1: .asciz "Enter the number of miles followed by hours, separated by a comma e.g. 20,1: "
+    formatStr: .asciz "%d,%d"
+    inMiles: .word 0
+    inHours: .word 0
+    outKilometers: .word 0
+    outKph: .word 0
+    outStr: .asciz "%d in kilometers is %d. Over %d hours, that is %d kilometers per hour. \n"
