20fd8e9d1cddd8bc0a1008096cc7aacc9f0e4ea3
karnika <arorakarnika@gmail.com>
2023-07-30 22:21:41 -0400

    add findmax program

---
+++ assignments/module9/findMaxOf3.s
@@ -0,0 +1,118 @@
+# Program Name: findMax.s
+# Author: Karnika Arora
+# Date: 07/30/2023
+# Module 9 Assignment #3
+# Purpose: Implement a function to find the largest of 3 values.  
+#   The function signature is "findMaxOf3(int val1, int val2, int val3)".  
+#   The function should compare the 3 values, and return the largest.  
+#   Be sure to use the ABI standards for all arguments and return values.  
+#   Write a program to prompt for 3 values, call this function to the find the maximum value, and print out the maximum value.  Follow proper style for assembly.
+# Functions: main, findMaxOf3
+
+.global main
+.global findMaxOf3
+
+# START MAIN FUNCTION
+.text
+main:
+    # Push the stack
+    SUB sp, sp, #4 
+    STR lr, [sp, #0] 
+
+    // Prompt for input values to compare
+    LDR r0, =promptInput
+    BL printf
+
+    // Read in the input values
+    LDR r0, =inputFormat
+    LDR r1, =val1
+    LDR r2, =val2
+    LDR r3, =val3
+    BL scanf
+
+    // Call the findMaxOf3 function
+    LDR r0, =val1
+    LDR r0, [r0]
+    LDR r1, =val2
+    LDR r1, [r1]
+    LDR r2, =val3
+    LDR r2, [r2]
+    BL findMaxOf3
+
+    // Print the max value
+    MOV r1, r0 // this is the max value from the findMaxOf3 function
+    LDR r0, =result
+    BL printf
+
+     # Pop the stack and return
+    LDR lr, [sp, #0] 
+    ADD sp, sp, #4 
+    MOV pc, lr 
+
+.data
+    promptInput: .asciz "Enter 3 comma separated values: "
+    inputFormat: .asciz "%d,%d,%d"
+    val1: .word 0
+    val2: .word 0
+    val3: .word 0
+    result: .asciz "The max value is %d\n"
+
+
+# END MAIN FUNCTION
+
+
+# START findMaxOf3 FUNCTION
+.text
+findMaxOf3:
+    # Push the stack
+    SUB sp, sp, #4 
+    STR lr, [sp, #0] 
+
+    // Compare the 3 values
+    MOV r3, #0 // logical value to keep track of if val1 is greater than val2
+    CMP r0, r1
+    ADDGE r3, r3, #1 // if val1 is greater than val2, r3 = 1, and we move on to comparing it to val3
+    CMP r3, #1
+    BEQ compareVal1Val3 // if val1 is greater than val2, compare it to val3
+        // if val1 < val2, it can't be the max: compare val2 to val3
+        // if val3 is greater than val2, val3 is the max
+        // if val2 is greater than val3, val2 is the max
+        B endVal1Val3Comparison
+    compareVal1Val3:
+        MOV r3, #0 // logical value to keep track of if val1 is greater than val3
+        CMP r0, r2
+        ADDGE r3, r3, #1 // if val1 is greater than val3, r3 = 1, and it is the max
+        CMP r3, #1
+        BEQ val1Max
+            // if val1 < val3, but val1 > val2, val3 is the max
+            B val3Max
+    endVal1Val3Comparison:
+    // val1 < val2, so compare val2 to val3
+    MOV r3, #0 // logical value to keep track of if val2 is greater than val3
+    CMP r1, r2
+    ADDGE r3, r3, #1 // if val2 is greater than val3, r3 = 1, and it is the max
+    CMP r3, #1
+    BEQ val2Max
+        // if val2 < val3, but val1 < val2, val3 is the max
+        B val3Max
+
+    val1Max:
+        MOV r0, r0
+        B endFindMaxOf3
+    val2Max:
+        MOV r0, r1
+        B endFindMaxOf3
+    val3Max:
+        MOV r0, r2
+        B endFindMaxOf3
+    
+    endFindMaxOf3:
+
+    # Pop the stack and return
+    LDR lr, [sp, #0] 
+    ADD sp, sp, #4 
+    MOV pc, lr 
+
+.data
+
+# END findMaxOf3 FUNCTION
--- assignments/module9/gradingProgram.s
+++ assignments/module9/gradingProgram.s
@@ -7,9 +7,10 @@
 #           - If the average is <0 or >100,  print an error
 #           - Else calculate a grade as 90-100 as A, 80-90 as B, 70-80 as C, else F.
 #           - Print out the student's name and grade.
-# Functions: main, 
+# Functions: main, calculateGrades
 
 .global main
+.global calculateGrades
 
 # START MAIN FUNCTION
 .text
@@ -18,50 +19,32 @@ main:
     SUB sp, sp, #4 
     STR lr, [sp, #0] 
 
-    // Prompt for input
-    LDR r0, =prompt
+    // Prompt for name input
+    LDR r0, =promptName
     BL printf
 
-    // Read input
-    LDR r0, =inputFormat
+    // Read input name
+    LDR r0, =inputFormatName
     LDR r1, =inputName
-    LDR r2, =inputAverage
     BL scanf
 
-    // Check if average is valid, raise an error if not
-    MOV r0, #0 // store the logical value to check if the input is valid in r0
+    // Prompt for average input
+    LDR r0, =promptAverage
+    BL printf
+
+    // Read input average
+    LDR r0, =inputFormatAverage
     LDR r1, =inputAverage
-    LDR r1, [r1]
-    CMP r0, #0
-    BLT error
-    CMP r0, #100
-    BGT error
-    error:
-        LDR r0, =errorOutput
-        BL printf
-        B exit
+    BL scanf
 
-    // Calculate grade
-    MOV r0, #0 # logical value set to true if the grade is A
+    // Call CalculateGrades function
     LDR r1, =inputAverage
-    LDR r1, [r1]
-    CMP r1, #90
-    BGE gradeA
-
-    CMP r1, #80
-    BGE gradeB
-
-    CMP r1, #70
-    BGE gradeC
-    CMP r1, #70
-    BLE gradeF
+    BL calculateGrades
 
     // Print grade
+    MOV r2, r0 // move grade from calculateGrades function into r2
     LDR r0, =resultOutput
     LDR r1, =inputName
-    LDR r1, [r1]
-    LDR r2, =grade
-    LDR r2, [r2]
     BL printf
 
     # Pop the stack and return
@@ -70,33 +53,86 @@ main:
     MOV pc, lr 
 
 .data
-    prompt: .asciz "Enter a name and an average separated by a comma, e.g. 'Karnika, 90': "
-    inputFormat: .asciz "%s, %d"
-    inputName: .word 0
-    inputAverage: .word 0
-    errorOutput: .asciz "Error: Average must be between 0 and 100"
-    grade: .word 0
-    resultOutput: .asciz "Student %s has a grade of %c\n"
+    promptName: .asciz "Enter a the student's name: "
+    promptAverage: .asciz "Enter the student's average: "
+    inputFormatName: .asciz "%s"
+    inputName: .space 40
+    inputFormatAverage: .asciz "%d"
+    inputAverage: .word 0   
+    resultOutput: .asciz "Student %s has a grade of %s\n"
 
 # END MAIN FUNCTION
 
+# START calculateGrades FUNCTION
 .text
-gradeA:
-    MOV r0, 0x41
-    LDR r1, =grade
-    STR r0, [r1]
+calculateGrades:
+    # Push the stack
+    SUB sp, sp, #4 
+    STR lr, [sp, #0] 
 
-gradeB:
-    MOV r0, 0x42
-    LDR r1, =grade
-    STR r0, [r1]
+    // Check if average is valid, raise an error if not
+    MOV r0, #0
+    LDR r1, [r1] // this is the inputAverage
+    CMP r1, #0
+    ADDGE r0, r0, #1
+    MOV r2, #0
+    CMP r1, #100
+    ADDLE r2, r2, #1
+    AND r0, r0, r2
+    CMP r0, #1
+    BNE printError
+        # Continue if code is valid
+        B endError
+    printError:
+        LDR r0, =errorOutput
+        BL printf
+        B exit
+    
+    endError:
+    // Calculate grade if grade is valid
+    MOV r0, #0 // logical value set to true if the grade is A
+    CMP r1, #90
+    BGE gradeA
+        // Check next grade tier - B
+        CMP r1, #80
+        BGE gradeB
+            // If false, check next grade tier - C
+            CMP r1, #70
+            BGE gradeC
+                // if false, grade is F
+                CMP r1, #70
+                BLE gradeF
 
-gradeC:
-    MOV r0, 0x43
-    LDR r1, =grade
-    STR r0, [r1]
+    gradeA:
+        MOV r0, #0x41
+        LDR r2, =grade
+        STR r0, [r2]
+        B endGrade
+    gradeB:
+        MOV r0, #0x42
+        LDR r2, =grade
+        STR r0, [r2]
+        B endGrade
+    gradeC:
+        MOV r0, #0x43
+        LDR r2, =grade
+        STR r0, [r2]
+        B endGrade
+    gradeF:
+        MOV r0, #0x46
+        LDR r2, =grade
+        STR r0, [r2]
+        B endGrade
+    endGrade:
+        LDR r0, =grade
 
-gradeF:
-    MOV r0, 0x46
-    LDR r1, =grade
-    STR r0, [r1]
+
+    # Pop the stack and return
+    LDR lr, [sp, #0] 
+    ADD sp, sp, #4 
+    MOV pc, lr 
+
+.data
+    errorOutput: .asciz "Error: Average must be between 0 and 100. You entered %d\n"
+    grade: .word 0
+@ # END calculateGrades FUNCTION
