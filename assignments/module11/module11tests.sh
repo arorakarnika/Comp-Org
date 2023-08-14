4e5141b598c723316c19f0be9d9a18cc50f2e272
karnika <arorakarnika@gmail.com>
2023-08-13 20:29:16 -0400

    add tests

--- assignments/module11/module11tests.sh
+++ assignments/module11/module11tests.sh
@@ -6,4 +6,4 @@ echo `make All`
 
 # Run tests, runTests.sh should be two directories up from the current directory
 ../../runTestsListInput.sh -n recursiveMultiply -t "5,4, 90,10, 7,45, -1,9, 0,4"
-../../runTestsListInput.sh -n calculateFibonacci -t "5, 4, 10, 19, 17, 0, 1"
+../../runTestsListInput.sh -n calculateFibonacci -t "5, 4, 10, 19, 17, 0, 1, -3"
